38a72a9f75714f91610fac8381485c221923d526
sergiosanz <sanzestradasergio@gmail.com>
2017-05-02 11:56:01 +0200

    Nuevo Solitario

--- Solitario/src/solitario/Carta.java
+++ Solitario/src/solitario/Carta.java
@@ -4,44 +4,46 @@ import java.awt.*;
 import java.applet.*;
 
 public class Carta extends Rectangle{
-
-	public static final int ANCHURA = 100;
-	public static final int ALTURA = 200;
-	public static final int NEGRO = 1;
-	public static final int ROJO = 2;
-	public static final int TREBOLES = 0;
-	public static final int DIAMANTES = 1;
-	public static final int CORAZONES = 2;
-	public static final int PICAS= 3;
+	public static final int ANCHURA=70;
+	public static final int ALTURA=120;
+	public static final int ROJO=1;
+	public static final int NEGRO=2;
+	public static final int PICAS=0;
+	public static final int ROMBOS=1;
+	public static final int CORAZONES=2;
+	public static final int TREBOLES=3;
 	private Image imagen;
 	private int valor;
 	private int color;
 	private int palo;
 	
-	public Carta(Image img,int v, int p, int c) {
-		super(-200, -200, ANCHURA, ALTURA);
+	
+	public Carta(Image img,int v, int c, int p) {
+		super(-200,-200,ANCHURA,ALTURA);
 		imagen=img;
-		valor = v;
-		color = c;
-		palo = p;
+		valor=v;
+		color=c;
+		palo=p;
 	}
 	public void dibujar(Graphics g, Applet a){
-		g.drawImage(imagen, 100, 200,a);
+		g.drawImage(imagen, x, y, 100, 200,a);
 	}
-	public int getValor() {
-		return valor;
+	public void setPosx(int px){
+		x=px;
 	}
-
-	public int getColor() {
-		return color;
+	public void setPosy(int py){
+		y=py;
 	}
-
-	public int getPalo() {
-		return palo;
-	}
-	public Image getImagen() {
+	public Image get_image(){
 		return imagen;
 	}
-
+	public int getvalor(){
+		return valor;
+	}
+	public int getcolor(){
+		return color;
+	}
+	public int getpalo(){
+		return palo;
+	}
 }
-
--- Solitario/src/solitario/Solitario.java
+++ Solitario/src/solitario/Solitario.java
@@ -1,43 +1,48 @@
 package solitario;
 
-import java.applet.Applet;
-import java.awt.BorderLayout;
-import java.awt.Button;
-import java.awt.Color;
-import java.awt.Event;
-import java.awt.Graphics;
-import java.awt.Image;
-import java.awt.Label;
-import java.awt.Panel;
-import java.awt.TextField;
+import java.awt.*;
+import java.applet.*;
 
-public class Solitario extends Applet {
+	public class Solitario extends Applet{
+		public static final int NUM_CARTAS = 52;
+		public static final int CPP=13;
+		String nombres[]={"_of_clubs.png","_of_diamonds.png","_of_hearts.png","_of_spades.png"};
+		Image imgReverso;
+		Rectangle reverso;
+		Image imagenes[];
+		Baraja baraja;
+		java.util.List<Carta> cartasExtraidas;
+		public void init() {
+				
+			 	imagenes = new Image[NUM_CARTAS];
+		    	for(int i=0; i<4; i++)
+		    		for(int j=0;j<CPP;j++)
+		    			imagenes[(i*CPP)+j]=getImage(getCodeBase(),"cartas/"+(j+1)+nombres[i]);
+		    	imgReverso=getImage(getCodeBase(),"cartas/reverso.png");
+		    	reverso=new Rectangle(20,20,70,120);
+		      	baraja=new Baraja(imagenes);
+		    	baraja.barajar();
+		    	cartasExtraidas=new java.util.ArrayList();
+		    }
+		    
+		public void paint(Graphics g){
+			g.setColor(Color.green);
+			g.fillRect(0, 0, 900, 700);
+			g.drawImage(imgReverso , 20, 20, Carta.ANCHURA, Carta.ALTURA, this);
+			for (int i = 0; i < cartasExtraidas.size(); i++) {
+				cartasExtraidas.get(i).dibujar(g,this);
+			}
+			
 
-	public static final int NUM_CARTAS = 52;
-	public static final int CPP = 13;
-	String nombres[] = { "_of_clubs.png", "_of_diamonds.png", "_of_hearts.png", "_of_spades.png" };
-	Image imagenes[];
-	Image imgReverso;
-	Baraja baraja;
-
-	public void init() {
-
-		imagenes = new Image[NUM_CARTAS];
-		imgReverso = getImage(getCodeBase(),"cartas/reverso.png");
-		for (int i = 0; i < 4; i++)
-			for (int j = 0; j < CPP; j++)
-				imagenes[(i * CPP) + j] = getImage(getCodeBase(),"cartas/reverso.png");
-		baraja = new Baraja(imagenes);
-		baraja.barajar();
 	}
-	
-	public void paint(Graphics g){
-		g.setColor(Color.green);
-		g.fillRect(0, 0, 700, 700);
-		g.drawImage(imgReverso, 20, 20, 70, 120, this);
-	}
-	
-	public boolean action(Event ev, Object obj){
+		public boolean mouseDown(Event ev, int x, int y){
+			if(reverso.contains(x,y)){
+				cartasExtraidas.add(baraja.sacar());
+				cartasExtraidas.get(cartasExtraidas.size()-1).setPosx(110);
+				cartasExtraidas.get(cartasExtraidas.size()-1).setPosy(20);
+				repaint();
+			}
+			return true;
+		}
 		
-	}
-}
+}	
